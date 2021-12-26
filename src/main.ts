c15214a432d55d4053a1b4a7148cd0bcab035f2d
Gleb Bashkatov <glebbash@gmail.com>
2021-12-26 21:59:47 +0200

    feat: make hello world interpretable

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import * as child_process from 'child_process';
+import { unlink } from 'fs/promises';
 import {
-  APInt,
   BasicBlock,
-  ConstantInt,
-  Function,
+  Function as LLVMFunction,
+  FunctionCallee,
   FunctionType,
   IRBuilder,
   LLVMContext,
@@ -15,55 +15,89 @@ import { promisify } from 'util';
 
 const exec = promisify(child_process.exec);
 
-async function main() {
+async function main(mode: 'compile' | 'interpret' = 'compile') {
   const context = new LLVMContext();
+
+  const module = buildMainModule(context);
+
+  const llvmIRFile = 'output.ll';
+  module.print(llvmIRFile);
+
+  if (mode === 'compile') {
+    await compileLlvmIR(llvmIRFile, 'output');
+    await unlink(llvmIRFile);
+  } else {
+    await interpretLlvmIR(llvmIRFile);
+  }
+}
+
+function buildMainModule(context: LLVMContext): Module {
   const module = new Module('top', context);
   const builder = new IRBuilder(context);
 
   module.setTargetTriple('x86_64-pc-linux-gnu');
 
-  // create main function
-  const funcType = FunctionType.get(builder.getInt32Ty(), false);
-  const mainFunc = Function.Create(
-    funcType,
-    Function.LinkageTypes.ExternalLinkage,
-    'main',
-    module,
-  );
-  const entry = BasicBlock.Create(context, 'entrypoint', mainFunc);
-  builder.SetInsertPoint(entry);
-
-  // // string constant
-  const helloWorldStr = builder.CreateGlobalStringPtr('Hello World!');
-
-  // create "puts" function
-  const putsArgs = [builder.getInt8PtrTy()];
-  const putsType = FunctionType.get(builder.getInt32Ty(), putsArgs, false);
-  const putsFunc = module.getOrInsertFunction('puts', putsType);
-
-  // invoke it
-  builder.CreateCall(putsFunc, [helloWorldStr]);
-
-  // return zero
-  builder.CreateRet(ConstantInt.get(context, new APInt(32, 0)));
-
-  if (verifyFunction(mainFunc)) {
-    console.error('Verifying function failed');
-    return;
-  }
+  buildMainFunction(context, module, builder);
 
   if (verifyModule(module)) {
     console.error('Verifying module failed');
-    return;
+    process.exit(1);
   }
 
-  module.print('output.ll');
+  return module;
+}
 
-  await compileLlvmIR('output.ll', 'output');
+function buildMainFunction(
+  context: LLVMContext,
+  module: Module,
+  builder: IRBuilder,
+): LLVMFunction {
+  const funcType = FunctionType.get(builder.getInt32Ty(), false);
+  const mainFunc = LLVMFunction.Create(
+    funcType,
+    LLVMFunction.LinkageTypes.ExternalLinkage,
+    'main',
+    module,
+  );
+
+  const entry = BasicBlock.Create(context, 'entrypoint', mainFunc);
+  builder.SetInsertPoint(entry);
+
+  // string constant
+  const helloWorldStr = builder.CreateGlobalStringPtr('Hello World!');
+
+  const putsFunc = defineExternPuts(builder, module);
+  builder.CreateCall(putsFunc, [helloWorldStr]);
+
+  // return zero
+  builder.CreateRet(builder.getInt32(0));
+
+  if (verifyFunction(mainFunc)) {
+    console.error('Verifying function failed');
+    process.exit(1);
+  }
+
+  return mainFunc;
+}
+
+function defineExternPuts(builder: IRBuilder, module: Module): FunctionCallee {
+  const putsArgs = [builder.getInt8PtrTy()];
+  const putsType = FunctionType.get(builder.getInt32Ty(), putsArgs, false);
+  return module.getOrInsertFunction('puts', putsType);
 }
 
 async function compileLlvmIR(inputFile: string, outputFile = 'output') {
-  await exec(`clang-13 -O3 -o ${outputFile} ${inputFile}`);
+  const res = await exec(`clang-13 -O3 -o ${outputFile} ${inputFile}`);
+
+  if (res.stderr) console.error(res.stderr.slice(0, -1));
+  if (res.stdout) console.log(res.stdout.slice(0, -1));
 }
 
-main();
+async function interpretLlvmIR(inputFile: string) {
+  const res = await exec(`lli-13 ${inputFile}`);
+
+  if (res.stderr) console.error(res.stderr.slice(0, -1));
+  if (res.stdout) console.log(res.stdout.slice(0, -1));
+}
+
+main('interpret');
