d760ac8dcfd4b2bd22d017c792c54743efff5b4a
Gleb Bashkatov <glebbash@gmail.com>
2021-12-29 18:26:20 +0200

    feat: add parser

---
+++ src/parser.spec.ts
@@ -0,0 +1,95 @@
+import { m } from 'multiline-str';
+
+import { parse } from './parser';
+
+describe('parser', () => {
+  it('parses empty list', () => {
+    const res = parse('()');
+
+    expect(res).toStrictEqual([[]]);
+  });
+
+  it('parses symbols', () => {
+    expect(parse('(a)')).toStrictEqual([['a']]);
+    expect(parse('(abc)')).toStrictEqual([['abc']]);
+  });
+
+  it('parses numbers', () => {
+    expect(parse('(0)')).toStrictEqual([['0']]);
+    expect(parse('(10)')).toStrictEqual([['10']]);
+    expect(parse('(10.0)')).toStrictEqual([['10.0']]);
+    expect(parse('(0.10)')).toStrictEqual([['0.10']]);
+    expect(parse('(100_000.000_000)')).toStrictEqual([['100_000.000_000']]);
+  });
+
+  it('parses strings', () => {
+    expect(
+      parse('("The quick brown fox jumps over the lazy dog.")'),
+    ).toStrictEqual([['"The quick brown fox jumps over the lazy dog."']]);
+    expect(parse('("\\"escaped quotes supported\\"")')).toStrictEqual([
+      ['"\\"escaped quotes supported\\""'],
+    ]);
+  });
+
+  it('parses lists with multiple atoms', () => {
+    const res = parse('(abc1 123 "string\\"s")');
+
+    expect(res).toStrictEqual([['abc1', '123', '"string\\"s"']]);
+  });
+
+  it('parses complex expressions', () => {
+    const res = parse('(a (b (c () d e f (g h)))) (i) (j k (l m n))');
+
+    expect(res).toStrictEqual([
+      ['a', ['b', ['c', [], 'd', 'e', 'f', ['g', 'h']]]],
+      ['i'],
+      ['j', 'k', ['l', 'm', 'n']],
+    ]);
+  });
+
+  it('parses llvm hello world', () => {
+    const res = parse(m`
+      (llvm/target-triple "x86_64-pc-linux-gnu")
+
+      (llvm/extern-fn puts (llvm/i8-ptr-ty) llvm/i32-ty)
+
+      (llvm/define-string-ptr helloWorldStr "Hello World!")
+
+      (llvm/fn main () (llvm/i32-ty)
+        (llvm/insert-point "entrypoint")
+        (llvm/call puts (helloWorldStr))
+        (llvm/ret (llvm/i32 0))
+      )
+      `);
+
+    expect(res).toStrictEqual([
+      ['llvm/target-triple', '"x86_64-pc-linux-gnu"'],
+      ['llvm/extern-fn', 'puts', ['llvm/i8-ptr-ty'], 'llvm/i32-ty'],
+      ['llvm/define-string-ptr', 'helloWorldStr', '"Hello World!"'],
+      [
+        'llvm/fn',
+        'main',
+        [],
+        ['llvm/i32-ty'],
+        ['llvm/insert-point', '"entrypoint"'],
+        ['llvm/call', 'puts', ['helloWorldStr']],
+        ['llvm/ret', ['llvm/i32', '0']],
+      ],
+    ]);
+  });
+
+  it('throws error on invalid input', () => {
+    expect(() => parse('')).toThrow(
+      `Unexpected character: '<EOF>' at line 1, col 1`,
+    );
+    expect(() => parse('(')).toThrow(
+      `Unexpected character: '<EOF>' at line 1, col 2`,
+    );
+    expect(() => parse('(a ()')).toThrow(
+      `Unexpected character: '<EOF>' at line 1, col 6`,
+    );
+    expect(() => parse('(a ))')).toThrow(
+      "Unexpected character: ')' at line 1, col 5",
+    );
+  });
+});
---
+++ src/parser.ts
@@ -0,0 +1,85 @@
+import {
+  chain,
+  char,
+  either,
+  EOF,
+  lazy,
+  map,
+  oneOrMany,
+  Parser,
+  pattern,
+  skip,
+  zeroOrMany,
+} from './combinator-parser';
+
+export type SExpr = string | SExpr[];
+type Pos = { line: number; col: number };
+
+class ParseError extends Error {
+  constructor(message: string, pos: Pos) {
+    super(`${message} at line ${pos.line}, col ${pos.col}`);
+  }
+}
+
+export function parse(input: string): SExpr[] {
+  const res = script(input);
+
+  if (!res.ok) {
+    const pos = getPosAtIndex(input, input.length - res.remaining.length);
+
+    throw new ParseError(res.error, pos);
+  }
+
+  if (res.remaining.length > 0) {
+    const pos = getPosAtIndex(input, input.length - res.remaining.length);
+
+    throw new ParseError(`Unexpected character '${res.remaining[0]}'`, pos);
+  }
+
+  return res.value;
+}
+
+function getPosAtIndex(input: string, index: number): Pos {
+  const pos: Pos = { line: 1, col: 1 };
+
+  for (let i = 0; i < index; i++) {
+    const char = input[i];
+
+    if (char === '\n') {
+      pos.line++;
+      pos.col = 0;
+    }
+
+    pos.col++;
+  }
+
+  return pos;
+}
+
+const whitespace = map(pattern(/\s*/), (c) => c.length);
+const atom = either([
+  pattern(/^[^()\d"][\w\/-]*/), // symbol
+  pattern(/^\d[\d_]*(?:\.[\d_]*)?/), // number
+  pattern(/^"(?:[^"\\]|\\.)*"/), // string
+]);
+const exprThenWhitespace: Parser<SExpr> = lazy(() =>
+  map(chain([expr, skip(whitespace)] as const), ([expr]) => expr),
+);
+const list = map(
+  chain([
+    skip(char('(')),
+    skip(whitespace),
+    zeroOrMany(exprThenWhitespace),
+    skip(char(')')),
+  ] as const),
+  ([content]) => content as SExpr,
+);
+const expr = either([atom, list]);
+const script = map(
+  chain([
+    skip(whitespace),
+    oneOrMany(map(chain([list, skip(whitespace)] as const), ([expr]) => expr)),
+    skip(char(EOF)),
+  ] as const),
+  ([exprs]) => exprs,
+);
