02a6b41eb90499b1017478b8dde7065a0ecd2a59
Gleb Bashkatov <glebbash@gmail.com>
2021-12-29 23:25:08 +0200

    feat(compiler): add support for constants and i32 values

--- src/compiler.ts
+++ src/compiler.ts
@@ -14,8 +14,19 @@ import { panic } from 'panic-fn';
 
 import { SExpr } from './parser';
 
+// TODO: pass expression locations to compiler for better error messages
+
 const STRING_START = ['"'];
 const NUMBER_START = '0123456789'.split('');
+
+type ModuleContext = {
+  context: LLVMContext;
+  builder: IRBuilder;
+  module: Module;
+  values: Record<string, Value>;
+};
+
+type FunctionContext = ModuleContext & { fn: LLVMFunction };
 
 export function compile(exprs: SExpr[], outputIRFile: string) {
   const context = new LLVMContext();
@@ -26,10 +37,15 @@ export function compile(exprs: SExpr[], outputIRFile: string) {
 }
 
 function buildModule(context: LLVMContext, exprs: SExpr[]): Module {
-  const module = new Module('top', context);
-  const builder = new IRBuilder(context);
+  const ctx: ModuleContext = {
+    context,
+    builder: new IRBuilder(context),
+    module: new Module('main', context),
+    values: {},
+  };
 
-  const values: Record<string, Value> = {};
+  ctx.values['true'] = ctx.builder.getTrue();
+  ctx.values['false'] = ctx.builder.getFalse();
 
   for (const expr of exprs) {
     const [command, ...args] = expr;
@@ -39,21 +55,21 @@ function buildModule(context: LLVMContext, exprs: SExpr[]): Module {
       const [targetTriple] = expectArgsLength(1, args, command);
       expectString(targetTriple);
 
-      module.setTargetTriple(getStringValue(targetTriple));
+      ctx.module.setTargetTriple(getStringValue(targetTriple));
     } else if (command === 'llvm/extern-fn') {
       const [fnName, argTypes, returnType] = expectArgsLength(3, args, command);
       expectSymbol(fnName);
       expectList(argTypes);
       expectSymbol(returnType);
 
-      module.getOrInsertFunction(
+      ctx.module.getOrInsertFunction(
         fnName,
         FunctionType.get(
-          getType(returnType, builder),
+          getType(returnType, ctx.builder),
           argTypes.map((argType) => {
             expectSymbol(argType);
 
-            return getType(argType, builder);
+            return getType(argType, ctx.builder);
           }),
           false,
         ),
@@ -63,11 +79,11 @@ function buildModule(context: LLVMContext, exprs: SExpr[]): Module {
       expectSymbol(stringName);
       expectString(stringContent);
 
-      values[stringName] = builder.CreateGlobalStringPtr(
+      ctx.values[stringName] = ctx.builder.CreateGlobalStringPtr(
         getStringValue(stringContent),
         stringName,
         0,
-        module,
+        ctx.module,
       );
     } else if (command === 'llvm/fn') {
       const [fnName, argTypes, returnType, ...exprs] = expectArgsLengthAtLeast(
@@ -80,98 +96,110 @@ function buildModule(context: LLVMContext, exprs: SExpr[]): Module {
       expectSymbol(returnType);
 
       const fnType = FunctionType.get(
-        getType(returnType, builder),
+        getType(returnType, ctx.builder),
         argTypes.map((argType) => {
           expectSymbol(argType);
 
-          return getType(argType, builder);
+          return getType(argType, ctx.builder);
         }),
         false,
       );
 
-      const fn = LLVMFunction.Create(
-        fnType,
-        LLVMFunction.LinkageTypes.ExternalLinkage,
-        fnName,
-        module,
-      );
-
-      for (const expr of exprs) {
-        const [command, ...args] = expr;
-        expectSymbol(command);
-
-        if (command === 'llvm/insert-point') {
-          const [entryName] = expectArgsLength(1, args, command);
-          expectString(entryName);
-
-          builder.SetInsertPoint(
-            BasicBlock.Create(context, getStringValue(entryName), fn),
-          );
-        } else if (command === 'llvm/define-string-ptr') {
-          const [stringName, stringContent] = expectArgsLength(
-            2,
-            args,
-            command,
-          );
-          expectSymbol(stringName);
-          expectString(stringContent);
-
-          values[stringName] = builder.CreateGlobalStringPtr(
-            getStringValue(stringContent),
-            stringName,
-            0,
-            module,
-          );
-        } else if (command === 'llvm/call') {
-          const [fnName, argValues] = expectArgsLength(2, args, command);
-          expectSymbol(fnName);
-          expectList(argValues);
-
-          const callee = module.getFunction(fnName);
-
-          if (!callee) {
-            panic(`Function ${fnName} is not defined`);
-          }
-
-          builder.CreateCall(
-            callee,
-            argValues.map((arg) => {
-              expectSymbol(arg);
-
-              const constant = values[arg];
-
-              if (!constant) {
-                panic(`Constant is not defined ${arg}`);
-              }
-
-              return constant;
-            }),
-          );
-        } else if (command === 'llvm/ret') {
-          const [returnExpr] = expectArgsLength(1, args, command);
-          returnExpr;
-
-          // TODO: implement returning values other than 0
-
-          builder.CreateRet(builder.getInt32(0));
-        } else {
-          panic(`Unknown command: ${command}`);
-        }
-      }
-
-      if (verifyFunction(fn)) {
-        panic(`Function verification failed: ${fnName}`);
-      }
+      buildFunction(fnName, fnType, exprs, ctx);
     } else {
       panic(`Unknown command: ${command}`);
     }
   }
 
-  if (verifyModule(module)) {
-    panic(`Verifying module failed: ${module.getName()}`);
+  if (verifyModule(ctx.module)) {
+    panic(`Verifying module failed: ${ctx.module.getName()}`);
   }
 
-  return module;
+  return ctx.module;
+}
+
+function buildFunction(
+  fnName: string,
+  fnType: FunctionType,
+  exprs: SExpr[],
+  moduleContext: ModuleContext,
+) {
+  const ctx: FunctionContext = {
+    ...moduleContext,
+    fn: LLVMFunction.Create(
+      fnType,
+      LLVMFunction.LinkageTypes.ExternalLinkage,
+      fnName,
+      moduleContext.module,
+    ),
+  };
+
+  for (const expr of exprs) {
+    const [command, ...args] = expr;
+    expectSymbol(command);
+
+    if (command === 'llvm/insert-point') {
+      const [entryName] = expectArgsLength(1, args, command);
+      expectString(entryName);
+
+      ctx.builder.SetInsertPoint(
+        BasicBlock.Create(ctx.context, getStringValue(entryName), ctx.fn),
+      );
+    } else if (command === 'llvm/call') {
+      const [fnName, argValues] = expectArgsLength(2, args, command);
+      expectSymbol(fnName);
+      expectList(argValues);
+
+      const callee = ctx.module.getFunction(fnName);
+
+      if (!callee) {
+        panic(`Function ${fnName} is not defined`);
+      }
+
+      ctx.builder.CreateCall(
+        callee,
+        argValues.map((arg) => buildValue(arg, ctx)),
+      );
+    } else if (command === 'llvm/ret') {
+      const [returnExpr] = expectArgsLength(1, args, command);
+
+      ctx.builder.CreateRet(buildValue(returnExpr, ctx));
+    } else {
+      panic(`Unknown command: ${command}`);
+    }
+  }
+
+  if (verifyFunction(ctx.fn)) {
+    panic(`Function verification failed: ${ctx.fn.getName()}`);
+  }
+}
+
+function buildValue(expr: SExpr, ctx: ModuleContext): Value {
+  if (isSymbol(expr)) {
+    const constant = ctx.values[expr];
+
+    if (!constant) {
+      panic(`Constant is not defined ${expr}`);
+    }
+
+    return constant;
+  }
+
+  expectList(expr);
+
+  const [command, ...args] = expr;
+
+  if (command === 'i32') {
+    const [value] = expectArgsLength(1, args, command);
+    expectNumber(value);
+
+    const i32Value = getNumberValue(value);
+    expectI32(i32Value);
+
+    return ctx.builder.getInt32(i32Value);
+  }
+
+  panic(`Unknown command: ${command}`);
 }
 
 function getType(typeName: string, builder: IRBuilder): Type {
@@ -183,6 +211,28 @@ function getType(typeName: string, builder: IRBuilder): Type {
     default:
       panic(`Unknown type: ${typeName}`);
   }
+}
+
+/*
+  TODO: merge assertions and type predicates
+  Would be nice to have it like this:
+  isSymbol(expr: SExpr): expr is string
+  expect(isSymbol): asserts expr is string
+ */
+function isSymbol(expr: SExpr): expr is string {
+  if (typeof expr !== 'string') {
+    return false;
+  }
+
+  if (STRING_START.includes(expr[0])) {
+    return false;
+  }
+
+  if (NUMBER_START.includes(expr[0])) {
+    return false;
+  }
+
+  return true;
 }
 
 function expectSymbol(expr: SExpr): asserts expr is string {
@@ -213,9 +263,30 @@ function expectString(expr: SExpr): asserts expr is string {
   }
 }
 
+function expectNumber(expr: SExpr): asserts expr is string {
+  if (typeof expr !== 'string') {
+    panic('Number expected, found: list');
+  }
+
+  if (STRING_START.includes(expr[0])) {
+    panic('Number expected, found: string');
+  }
+
+  if (!NUMBER_START.includes(expr[0])) {
+    panic('Number expected, found: symbol');
+  }
+}
+
 function expectList(expr: SExpr): asserts expr is SExpr[] {
   if (typeof expr === 'string') {
     panic('List expected, found: atom');
+  }
+}
+
+// TODO: add bound checks
+function expectI32(value: number) {
+  if (!Number.isInteger(value)) {
+    panic(`i32 can not hold ${value}`);
   }
 }
 
@@ -251,6 +322,10 @@ function getStringValue(value: string): string {
   return value
     .slice(1, -1)
     .replace(/\\"/g, '"')
-    .replace(/\\n/, '\n')
-    .replace(/\\\\/, '\\');
+    .replace(/\\n/g, '\n')
+    .replace(/\\\\/g, '\\');
+}
+
+function getNumberValue(value: string): number {
+  return parseFloat(value.replace(/_/g, ''));
 }
