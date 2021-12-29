c12d0c346cbb186a270c837f63fb60220c8bb875
Gleb Bashkatov <glebbash@gmail.com>
2021-12-29 21:46:27 +0200

    feat: switch to `lole` extension

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function main() {
   const args = process.argv;
 
   const mode = args.includes('-r') ? 'interpret' : 'compile';
-  const inputFile = getArg(args, 'src') ?? 'examples/hello-world.lll';
+  const inputFile = getArg(args, 'src') ?? 'examples/hello-world.lole';
   const outputIRFile = getArg(args, 'out-ir') ?? 'output.ll';
   const outputBinaryFile = getArg(args, 'out') ?? 'output';
 
