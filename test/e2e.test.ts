9e2e0a782140bc424631e616d368f88cb42d7e3e
glebbash <glebbash@gmail.com>
2022-06-28 14:36:50 +0000

    feat: restructure tests

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -1,112 +1,18 @@
 import { assertEquals } from "https://deno.land/std@0.123.0/testing/asserts.ts";
-import { m } from "https://raw.githubusercontent.com/glebbash/multiline-str/master/src/multiline-str.ts";
 
-const expectedResults = [
-  {
-    program: "examples/cmd-args.lole",
-    output: m`
-      Program name: -
+const exampleProgramsDir = "examples/";
+const expectedOutputsDir = "examples/_expected_outputs";
 
-      `,
-  },
-  {
-    program: "examples/fn.lole",
-    output: m`
-      Number: 4
+for await (const test of Deno.readDir(expectedOutputsDir)) {
+  if (!test.isFile) continue;
 
-      `,
-  },
-  {
-    program: "examples/get_set.lole",
-    output: m`
-      Pointer value: 1
-      Pointer value: 2
-      Pointer value in function: 2
-      Bob's id is 1
-      First element: 1
-      First element with helper: 1
-      Bottom-right matrix element: 9
+  const expectedOutputFile = `${expectedOutputsDir}/${test.name}`;
+  const program = exampleProgramsDir + test.name.slice(0, -".txt".length);
 
-      `,
-  },
-  {
-    program: "examples/hello-world.lole",
-    output: m`
-      Hello World!
-
-      `,
-  },
-  {
-    program: "examples/nested-if.lole",
-    output: m`
-      cmp: f
-
-      `,
-  },
-  {
-    program: "examples/parse-file.lole",
-    output: m`
-      (include "./lib/file.lole")
-
-      (external-fn puts ((& i8)) void)
-      (external-fn strtok ((& i8) (& i8)) (& i8))
-
-      (fn main () i32
-        (let source (read_file "examples/parse-file.lole"))
-        (puts source)
-
-        (puts "----------------------------")
-
-        (puts "first line:")
-        (let first_line (strtok source "\\n"))
-        (puts first_line)
-
-        (puts "second line:")
-        (let second_line (strtok (nullptr (& i8)) "\\n"))
-        (puts second_line)
-
-        0
-      )
-
-      ----------------------------
-      first line:
-      (include "./lib/file.lole")
-      second line:
-      (external-fn puts ((& i8)) void)
-
-      `,
-  },
-  {
-    program: "examples/rec.lole",
-    output: m`
-      1
-      2
-      3
-      4
-      5
-      6
-      7
-      8
-      9
-      10
-
-      `,
-  },
-  {
-    program: "examples/struct.lole",
-    output: m`
-      1
-      2
-      3
-
-      `,
-  },
-];
-
-for (const { program, output } of expectedResults) {
   Deno.test(`it executes ${program}`, async () => {
     const programOutput = await runProgram(program);
-    assertEquals(programOutput, output);
+    const expectedOutput = await Deno.readTextFile(expectedOutputFile);
+    assertEquals(programOutput, expectedOutput);
   });
 }
 
