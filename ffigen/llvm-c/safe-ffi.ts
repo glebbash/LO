9d0678af6856d3f3067ca04f6891aa183ce014bb
glebbash <glebbash@gmail.com>
2022-07-20 21:26:25 +0000

    chore: use deno-ffigen

--- ffigen/llvm-c/safe-ffi.ts
+++ ffigen/llvm-c/safe-ffi.ts
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file
 export type Opaque<BaseType, BrandType = unknown> = BaseType & {
   readonly [Symbols.base]: BaseType;
   readonly [Symbols.brand]: BrandType;
@@ -7,7 +8,6 @@ export type Pointer<T> = Opaque<bigint, T>;
 export type FnPointer<T> = Pointer<T>;
 export type StructPointer<T> = Pointer<T>;
 
-// deno-lint-ignore no-namespace
 namespace Symbols {
   export declare const base: unique symbol;
   export declare const brand: unique symbol;
