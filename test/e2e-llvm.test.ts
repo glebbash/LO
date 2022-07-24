bdc0f9f3623383f9aeec415d260ad1a384b7536d
glebbash <glebbash@gmail.com>
2022-07-24 10:22:45 +0000

    refactor: update test structure

--- test/e2e-llvm.test.ts
+++ test/e2e-llvm.test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std@0.123.0/testing/asserts.ts";
 
-const exampleProgramsDir = "examples/";
-const expectedOutputsDir = "examples/_expected_outputs";
+const exampleProgramsDir = "examples/llvm/";
+const expectedOutputsDir = "examples/llvm/_expected_outputs";
 
 for await (const test of Deno.readDir(expectedOutputsDir)) {
   if (!test.isFile) continue;
