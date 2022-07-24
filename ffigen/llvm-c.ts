a1980753e56f5583168a3c515151317e12ed798c
glebbash <glebbash@gmail.com>
2022-07-24 17:32:59 +0000

    chore: remove unused ffigen folder

--- ffigen/llvm-c.ts
+++
@@ -1,9 +0,0 @@
-import { loadLLVM } from "./llvm-c/mod.ts";
-
-const llvm = loadLLVM("./input/libLLVM-15git.so");
-
-const ctx = llvm.ContextCreate();
-
-console.log(ctx);
-
-llvm.ContextDispose(ctx);
