3aadf5d1ff84bef9bb3187cba6567619eaab5f08
glebbash <glebbash@gmail.com>
2023-02-27 19:15:36 +0000

    chore: init rust wasm project

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#[no_mangle]
+pub extern "C" fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
