ece772b8fc0e0760869dadcff1caa68ded39c259
glebbash <glebbash@gmail.com>
2023-02-28 18:02:09 +0000

    feat: complete wasm setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
+#![no_std]
+
 #[no_mangle]
-pub extern "C" fn add(left: usize, right: usize) -> usize {
-    left + right
+pub extern "C" fn compile(script: *const u8, script_len: usize) -> i32 {
+    let str = read_wasm_str(script, script_len).unwrap();
+    let res: i32 = str.parse().unwrap();
+    res
+}
+
+fn read_wasm_str<'a>(chars: *const u8, chars_len: usize) -> Option<&'a str> {
+    unsafe { core::str::from_utf8(core::slice::from_raw_parts(chars, chars_len)).ok() }
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
+    // if this throws compiler error, try: rustup target add wasm32-unknown-unknown
+    core::arch::wasm32::unreachable()
 }
 
 #[cfg(test)]
@@ -9,7 +24,14 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let (script, script_len) = build_wasm_str("1");
+
+        let result = compile(script, script_len);
+
+        assert_eq!(result, 1);
+    }
+
+    fn build_wasm_str(data: &str) -> (*const u8, usize) {
+        return (data.as_bytes().as_ptr(), data.as_bytes().len());
     }
 }
