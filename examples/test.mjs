301c684f00e74f6a6474d9a8dbe919acd80f8628
glebbash <glebbash@gmail.com>
2023-03-07 21:49:52 +0000

    feat: add external usage tests

---
+++ examples/test.mjs
@@ -0,0 +1,67 @@
+// @ts-check
+
+import { test } from "node:test";
+import assert from "node:assert";
+import { readFile } from "node:fs/promises";
+
+const COMPILER_PATH = "../target/wasm32-unknown-unknown/release/lole_lisp.wasm";
+
+const compiler = await loadWasm(await readFile(COMPILER_PATH));
+
+test("compiles 42 example", async () => {
+    const { ok, output } = await compile(compiler, await readFile("./42.lole"));
+
+    assert.strictEqual(ok, 1);
+
+    const program = await loadWasm(output);
+    const result = program.main();
+
+    assert.strictEqual(result, 42);
+});
+
+test("compiles factorial example", async () => {
+    const { ok, output } = await compile(
+        compiler,
+        await readFile("./factorial.lole")
+    );
+
+    assert.strictEqual(ok, 1);
+
+    const program = await loadWasm(output);
+    const result = program.factorial(5);
+
+    assert.strictEqual(result, 120);
+});
+
+// utils
+
+/**
+ * @param {BufferSource} data
+ * @returns {Promise<any>}
+ */
+async function loadWasm(data) {
+    const mod = await WebAssembly.instantiate(data);
+    return mod.instance.exports;
+}
+
+/**
+ * @param {any} compiler
+ * @param {Buffer} source
+ */
+async function compile(compiler, source) {
+    const inSize = source.byteLength;
+    const inPtr = compiler.mem_alloc(inSize);
+
+    const input = new Uint8Array(compiler.memory.buffer, inPtr, inSize);
+    input.set(source);
+
+    const [ok, outPtr, outSize] = compiler.compile(inPtr, inSize);
+
+    const output = new Uint8Array(outSize);
+    output.set(new Uint8Array(compiler.memory.buffer, outPtr, outSize));
+
+    compiler.mem_free(inPtr, inSize);
+    compiler.mem_free(outPtr, outSize);
+
+    return { ok, output };
+}
