7d1d61c780907185bb3f06a8c2efc82e8ae902d4
glebbash <glebbash@gmail.com>
2023-04-16 22:40:24 +0000

    fix: update vec impl

--- minify.js
+++ minify.js
@@ -3,13 +3,20 @@ const input = require("fs").readFileSync(0, { encoding: "utf8" });
 console.log(minify(input));
 
 function minify(input) {
+    while (true) {
+        const trimmed = input
+            .replace(/;.*\n/g, "")
+            .replace(/\s+/g, " ")
+            .replace(/([\(\{\[]) ([\(\{\[])/g, "$1$2")
+            .replace(/([\(\{\[]) ([\)\}\]])/g, "$1$2")
+            .replace(/([\)\}\]]) ([\)\}\]])/g, "$1$2")
+            .replace(/([\)\}\]]) ([\(\{\[])/g, "$1$2");
+
+        if (trimmed.length === input.length) break;
+        input = trimmed;
+    }
+
     return input
-        .replace(/;.*\n/g, "")
-        .replace(/\s+/g, " ")
-        .replace(/([\(\{\[]) ([\(\{\[])/g, "$1$2")
-        .replace(/([\(\{\[]) ([\)\}\]])/g, "$1$2")
-        .replace(/([\)\}\]]) ([\)\}\]])/g, "$1$2")
-        .replace(/([\)\}\]]) ([\(\{\[])/g, "$1$2")
         .replace(/\(export /g, "\n(export ")
         .replace(/\(fn /g, "\n(fn ")
         .replace(/\(struct /g, "\n(struct ")
