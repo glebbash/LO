0d488c0690b32d5cc7295c6cccf1924f36d0dd1e
glebbash <glebbash@gmail.com>
2023-10-01 21:46:51 +0000

    chore: use use get_lole_type everywhere

--- src/type_checker.rs
+++ src/type_checker.rs
@@ -123,115 +123,12 @@ pub fn get_lole_type(ctx: &BlockContext, instr: &LoleExpr) -> Result<LoleType, S
 }
 
 pub fn get_type(ctx: &BlockContext, instr: &LoleExpr) -> Result<Vec<WasmType>, CompileError> {
-    // TODO: use this for testing get_lole_type
-    // let lole_type = get_lole_type(ctx, instr).map_err(|message| CompileError {
-    //     message,
-    //     loc: Location::internal(),
-    // })?;
+    let lole_type = get_lole_type(ctx, instr).map_err(|message| CompileError {
+        message,
+        loc: Location::internal(),
+    })?;
 
-    // let mut wasm_types = vec![];
-    // lole_type.emit_components(ctx.module, &mut wasm_types);
-    // return Ok(wasm_types);
-
-    Ok(match instr {
-        LoleExpr::Unreachable => vec![],
-        LoleExpr::I32ConstLazy { value: _ } => vec![WasmType::I32],
-        LoleExpr::I32Const { value: _ } => vec![WasmType::I32],
-        LoleExpr::I64Const { value: _ } => vec![WasmType::I64],
-        LoleExpr::MultiValueEmit { values } => get_types(ctx, values)?,
-        LoleExpr::StructLoad {
-            primitive_loads,
-            struct_name: _,
-            address_instr: _,
-            address_local_index: _,
-            base_byte_offset: _,
-        } => get_types(ctx, primitive_loads)?,
-        LoleExpr::StructGet {
-            primitive_gets,
-            struct_name: _,
-            base_index: _,
-        } => get_types(ctx, primitive_gets)?,
-        LoleExpr::NoEmit { expr: instr } => get_type(ctx, instr)?,
-
-        LoleExpr::Casted {
-            value_type,
-            expr: _,
-        } => {
-            let mut wasm_types = vec![];
-            value_type.emit_components(ctx.module, &mut wasm_types);
-            wasm_types
-        }
-        LoleExpr::Set { bind: _ } => vec![],
-        LoleExpr::Drop {
-            value: _,
-            drop_count: _,
-        } => vec![],
-        LoleExpr::Return { value: _ } => vec![],
-        LoleExpr::MemorySize => vec![WasmType::I32],
-        LoleExpr::MemoryGrow { size: _ } => vec![WasmType::I32],
-
-        LoleExpr::BinaryOp { lhs, rhs, kind: _ } => {
-            get_type(ctx, rhs)?;
-            return get_type(ctx, lhs);
-        }
-        LoleExpr::Load {
-            kind,
-            align: _,
-            offset: _,
-            address_instr: _,
-        } => vec![kind.to_wasm_type().unwrap()],
-        LoleExpr::GlobalGet { global_index } => {
-            let wasm_global = ctx
-                .module
-                .wasm_module
-                .globals
-                .get(*global_index as usize)
-                .ok_or_else(|| CompileError::unreachable(file!(), line!()))?;
-
-            vec![wasm_global.kind.value_type]
-        }
-        LoleExpr::TypedLocalGet {
-            local_index,
-            value_type: _,
-        }
-        | LoleExpr::LocalGet { local_index } => {
-            let local_index = *local_index as usize;
-            let locals_len = ctx.fn_ctx.fn_type.inputs.len();
-            if local_index < locals_len {
-                vec![ctx.fn_ctx.fn_type.inputs[local_index]]
-            } else {
-                vec![ctx.fn_ctx.non_arg_locals[local_index - locals_len]]
-            }
-        }
-        LoleExpr::Call {
-            fn_index: _,
-            return_type,
-            args: _,
-        } => {
-            let mut wasm_outputs = vec![];
-            return_type.emit_components(ctx.module, &mut wasm_outputs);
-            wasm_outputs
-        }
-        LoleExpr::If {
-            block_type,
-            cond: _,
-            then_branch: _,
-            else_branch: _,
-        }
-        | LoleExpr::Block {
-            block_type,
-            body: _,
-        }
-        | LoleExpr::Loop {
-            block_type,
-            body: _,
-        } => {
-            if let Some(wasm_type) = block_type.to_wasm_type() {
-                vec![wasm_type]
-            } else {
-                vec![]
-            }
-        }
-        LoleExpr::Branch { label_index: _ } => vec![],
-    })
+    let mut wasm_types = vec![];
+    lole_type.emit_components(ctx.module, &mut wasm_types);
+    Ok(wasm_types)
 }
