fee26c2150508b145b952bc12bd75b16fa725882
glebbash <glebbash@gmail.com>
2024-02-13 20:26:44 +0200

    feat: implement aoc 2020 day 4 part 1 example

--- test.mjs
+++ test.mjs
@@ -328,12 +328,9 @@ test("compiles defer", async () => {
         assert.strictEqual(part2, "7540141059\n");
     });
 
-    test("aoc 2020 day 3", async () => {
+    test("aoc 2020 day 4", async () => {
         const part1 = await runAoc("./examples/aoc2020/4.lo");
-        assert.strictEqual(part1, "?\n");
-
-        const part2 = await runAoc("./examples/aoc2020/4-part2.lo");
-        assert.strictEqual(part2, "?\n");
+        assert.strictEqual(part1, "264\n");
     });
 
     async function runAoc(path) {
