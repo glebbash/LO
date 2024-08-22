d8530849f2cad686e821d1d7acfb78143fa185dd
glebbash <glebbash@gmail.com>
2024-08-23 01:47:14 +0300

    feat(v2): build LoExpr IR to support typechecking

--- src/ir_generator.rs
+++ src/ir_generator.rs
@@ -1,7 +1,7 @@
 use crate::{ast::*, core::*, parser_v2::*, wasm::*};
-use alloc::{format, string::String, vec::Vec};
+use alloc::{boxed::Box, format, string::String, vec::Vec};
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 enum LoType {
     U32,
 }
@@ -14,19 +14,75 @@ impl LoType {
     }
 }
 
+impl core::fmt::Display for LoType {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            LoType::U32 => f.write_str("u32"),
+        }
+    }
+}
+
+enum LoExpr {
+    U32Const { value: u32 },
+    Return { expr: Box<LoExpr> },
+    Add { lhs: Box<LoExpr>, rhs: Box<LoExpr> },
+    VarLoad { name: String, var_type: LoType },
+}
+
+impl LoExpr {
+    fn get_type(&self) -> LoType {
+        match self {
+            LoExpr::U32Const { .. } => LoType::U32,
+            LoExpr::Return { expr } => expr.get_type(),
+            LoExpr::Add { lhs, .. } => lhs.get_type(),
+            LoExpr::VarLoad { var_type, .. } => var_type.clone(),
+        }
+    }
+
+    fn lower(&self, scope: &LoScope, instrs: &mut Vec<WasmInstr>) {
+        match self {
+            LoExpr::U32Const { value } => instrs.push(WasmInstr::I32Const {
+                value: *value as i32,
+            }),
+            LoExpr::Return { expr } => {
+                expr.lower(scope, instrs);
+                instrs.push(WasmInstr::Return);
+            }
+            LoExpr::Add { lhs, rhs } => {
+                lhs.lower(scope, instrs);
+                rhs.lower(scope, instrs);
+                instrs.push(WasmInstr::BinaryOp {
+                    kind: WasmBinaryOpKind::I32_ADD,
+                })
+            }
+            LoExpr::VarLoad { name, .. } => {
+                let mut local_index = 0;
+                for var in &scope.variables {
+                    if var.name == *name {
+                        local_index = var.index;
+                    }
+                }
+
+                instrs.push(WasmInstr::LocalGet { local_index })
+            }
+        }
+    }
+}
+
 #[derive(Default)]
 pub struct IRGenerator {
     wasm_module: WasmModule,
     pub errors: LoErrorManager,
 }
 
-struct Variable {
+struct LoVariable {
     name: String,
+    type_: LoType,
     index: u32,
 }
 
-struct CodeScope {
-    locals: Vec<Variable>,
+struct LoScope {
+    variables: Vec<LoVariable>,
 }
 
 impl IRGenerator {
@@ -43,7 +99,9 @@ impl IRGenerator {
     }
 
     fn process_fn_def(&mut self, fn_def: &FnDefExpr) -> Result<(), LoError> {
-        let mut scope = CodeScope { locals: Vec::new() };
+        let mut scope = LoScope {
+            variables: Vec::new(),
+        };
 
         let mut fn_type = WasmFnType {
             inputs: Vec::new(),
@@ -53,9 +111,9 @@ impl IRGenerator {
         let return_type = self.build_type(&fn_def.return_type)?;
         return_type.emit_components(&mut fn_type.outputs);
 
-        for (fn_param, index) in fn_def.fn_params.iter().zip(0..) {
-            for local in &scope.locals {
-                if local.name == fn_param.name {
+        for fn_param in &fn_def.fn_params {
+            for var in &scope.variables {
+                if var.name == fn_param.name {
                     self.errors.report(LoError {
                         message: format!("Duplicate function parameter name: {}", fn_param.name),
                         loc: fn_param.loc.clone(),
@@ -64,12 +122,15 @@ impl IRGenerator {
                 }
             }
 
+            let local_index = fn_type.inputs.len() as u32;
+
             let param_type = self.build_type(&fn_param.type_)?;
             param_type.emit_components(&mut fn_type.inputs);
 
-            scope.locals.push(Variable {
+            scope.variables.push(LoVariable {
                 name: fn_param.name.clone(),
-                index: index as u32,
+                type_: param_type,
+                index: local_index,
             });
         }
 
@@ -129,12 +190,13 @@ impl IRGenerator {
 
     fn build_code_block(
         &mut self,
-        scope: &CodeScope,
+        scope: &LoScope,
         out_instrs: &mut Vec<WasmInstr>,
         code_block: &CodeBlockExpr,
     ) -> Result<(), LoError> {
         for expr in &code_block.exprs {
-            self.build_code_expr(scope, out_instrs, expr)?;
+            let lo_expr = self.build_code_expr(scope, expr)?;
+            lo_expr.lower(scope, out_instrs);
         }
 
         Ok(())
@@ -142,14 +204,16 @@ impl IRGenerator {
 
     fn build_code_expr(
         &mut self,
-        scope: &CodeScope,
-        out_instrs: &mut Vec<WasmInstr>,
+        scope: &LoScope,
         code_expr: &CodeExpr,
-    ) -> Result<(), LoError> {
+    ) -> Result<LoExpr, LoError> {
         match code_expr {
             CodeExpr::Return(return_expr) => {
-                self.build_code_expr(scope, out_instrs, &return_expr.expr)?;
-                out_instrs.push(WasmInstr::Return);
+                let lo_expr = self.build_code_expr(scope, &return_expr.expr)?;
+
+                Ok(LoExpr::Return {
+                    expr: Box::new(lo_expr),
+                })
             }
             CodeExpr::IntLiteral(int_expr) => {
                 let value = u32::from_str_radix(&int_expr.value, 10).map_err(|_| LoError {
@@ -157,35 +221,43 @@ impl IRGenerator {
                     loc: int_expr.loc.clone(),
                 })?;
 
-                out_instrs.push(WasmInstr::I32Const {
-                    value: value as i32,
-                })
+                Ok(LoExpr::U32Const { value })
             }
             CodeExpr::VarLoad(var_load) => {
-                for local in &scope.locals {
-                    if local.name == var_load.name {
-                        out_instrs.push(WasmInstr::LocalGet {
-                            local_index: local.index,
+                for var in &scope.variables {
+                    if var.name == var_load.name {
+                        return Ok(LoExpr::VarLoad {
+                            name: var.name.clone(),
+                            var_type: var.type_.clone(),
                         });
-                        return Ok(());
                     }
                 }
 
-                return Err(LoError {
+                Err(LoError {
                     message: format!("Cannot read unknown variable: {}", var_load.name),
                     loc: var_load.loc.clone(),
-                });
+                })
             }
-            CodeExpr::Add(AddExpr { lhs, rhs, .. }) => {
-                self.build_code_expr(scope, out_instrs, lhs)?;
-                self.build_code_expr(scope, out_instrs, rhs)?;
-                out_instrs.push(WasmInstr::BinaryOp {
-                    kind: WasmBinaryOpKind::I32_ADD,
-                });
-                return Ok(());
+            CodeExpr::Add(AddExpr { lhs, rhs, loc }) => {
+                let lo_lhs = self.build_code_expr(scope, lhs)?;
+                let lo_rhs = self.build_code_expr(scope, rhs)?;
+
+                let lhs_type = lo_lhs.get_type();
+                let rhs_type = lo_rhs.get_type();
+                if lhs_type != rhs_type {
+                    return Err(LoError {
+                        message: format!(
+                            "Operands have different types: {lhs_type} and {rhs_type}"
+                        ),
+                        loc: loc.clone(),
+                    });
+                }
+
+                Ok(LoExpr::Add {
+                    lhs: Box::new(lo_lhs),
+                    rhs: Box::new(lo_rhs),
+                })
             }
         }
-
-        Ok(())
     }
 }
